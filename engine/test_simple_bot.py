508df39c7a79b0f97cb38e2b926ba63611645d0a
Vu Anh <anhv.ict91@gmail.com>
2018-06-13 14:29:58 +0700

    rewrite tests

--- engine/test_simple_bot.py
+++ engine/test_simple_bot.py
@@ -4,49 +4,56 @@ from engine.simple_bot import SimpleBot
 def tests():
     print("$ python chatbot_test.py")
     SIMPLE = [
-        "bạn tên gì",
-        "mày tên là gì ?",
-        "bạn bao nhiêu tuổi",
-        "bạn sống ở đâu",
-        "bạn là ai",
-        "bạn tên gì",  # reask
-        "bạn tên gì",  # reask
-        "cậu bao nhiêu tuổi?",
-        "cậu bao nhiêu tuổi",
-        "bạn mấy tuổi",
-        "cậu bao nhiêu tuổi rồi ?",
-        "cậu là ai",
+
         "kém quá",
         "giỏi thật",
-        "Hoài An",
-        "Hoài An à",
         "mẹ mày",
         "như cứt"
     ]
+    GREETING_HELLO = ["hi"]
+    GREETING_BYE = ["tạm biệt"]
+    GREETING_HRU = ["Bạn có khỏe không", "khỏe không?"]
+    LAUGH = ["hihi", "hi hi"]
+    APOLOGY = ["xin lỗi"]
     CURSE = [
         "đồ ngu",
         "bot ngu"
     ]
-    INFORMATION = [
+    ASK_NAME = [
+        "bạn là ai", "cậu là ai",
+        "bạn tên gì",  "bạn tên gì",  # reask
+        "mày tên là gì ?",
+        "Hoài An", "Hoài An à",
+    ]
+    ASK_GENDER = [
         "cậu là nam hay nữ vậy",
         "cậu là nam hay nữ thế",
-        "cậu là nam hay nữ"
+        "cậu là nam hay nữ", "nam hay nữ"
     ]
-    APOLOGY = ["xin lỗi"]
-    GREETING = ["tạm biệt"]
+    ASK_AGE = [
+        "bạn bao nhiêu tuổi",
+        "cậu bao nhiêu tuổi?",
+        "bạn mấy tuổi",
+        "cậu bao nhiêu tuổi rồi ?",
+    ]
+    ASK_LOCATION = ["bạn sống ở đâu"]
     AGREEMENT = ["ok"]
     HACK = ["a", "a", "a", "b", "a", "a", "a", "a", "a", "a", "xin lỗi"]
     MASTER = ["có biết anh vũ anh không",
               "có biết anh vũ anh không?",
               "vũ anh là ai"]
-    SPECIAL_CHARACTERS = [
+    EXCEPTION = [
         ":))", "=)", "b"
     ]
     COLLECTION = [
-        SIMPLE, INFORMATION,
-        GREETING, MASTER, HACK,
-        CURSE, APOLOGY, AGREEMENT,
-        SPECIAL_CHARACTERS
+        GREETING_HELLO, GREETING_BYE, GREETING_HRU,
+        LAUGH,
+        AGREEMENT, APOLOGY,
+        SIMPLE,
+        ASK_NAME, ASK_GENDER, ASK_AGE, ASK_LOCATION, MASTER ,
+        HACK,
+        CURSE,
+        EXCEPTION
     ]
     # COLLECTION = [
     #     INFORMATION
