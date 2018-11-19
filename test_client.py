f35fc28b94b71b06ba3973c44118244a421d03ef
Vu Anh <anhv.ict91@gmail.com>
2018-11-19 17:39:36 +0700

    update chatbot

--- test_client.py
+++ test_client.py
@@ -3,11 +3,12 @@ from importlib import reload
 from client import Chatbot
 
 chatbot = Chatbot()
+chatbot.response(":build HoaiAn")
 
 chatbot.response(":reset")
 output = chatbot.response("may tuoi")
 print(output)
 
 chatbot.response(":reset")
-output = chatbot.response("cậu bao nhiêu tuổi")
+output = chatbot.response("ten gi")
 print(output)
