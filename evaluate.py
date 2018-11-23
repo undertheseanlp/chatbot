44774d2df49745a6d7c092dc624dc7c927a0b1bc
Vu Anh <anhv.ict91@gmail.com>
2018-11-23 17:20:22 +0700

    add sport

--- evaluate.py
+++ evaluate.py
@@ -1,35 +1,40 @@
 import json
 from os import listdir
 from os.path import join
-
+import pandas as pd
 from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 from engine.hoaian import HoaiAn
 import numpy as np
 import warnings
+
 warnings.filterwarnings("ignore")
+
 
 def scoring(question, answers):
     bot_answer = HoaiAn.reply("local", question)
     if len(answers) == 0:
         answer = ""
         return 0, bot_answer, answer
-    bleus = [(answer, sentence_bleu([bot_answer.lower().split()], answer.lower().split(), weights=[1, 0, 0, 0])) for answer in answers]
+    bleus = [(answer, sentence_bleu([bot_answer.lower().split()], answer.lower().split(), weights=[1, 0, 0, 0])) for
+             answer in answers]
     answer, bleu = max(bleus, key=lambda x: x[1])
     return bleu, bot_answer, answer
 
 
-def evaluate(filepath):
+def evaluate_file(filepath):
+    print("")
+    print(filepath)
     talks = json.load(open(filepath))
-    count = {
+    output = {
         "talk": 0,
         "question": 0
     }
     scores = []
     for talk in talks:
-        count["talk"] += 1
+        output["talk"] += 1
         questions = talk["question"]
         for question in questions:
-            count["question"] += 1
+            output["question"] += 1
             bleu, bot_answer, answer = scoring(question, talk["answer"])
             scores.append(bleu)
             if bleu < 0.8:
@@ -38,23 +43,24 @@ def evaluate(filepath):
                 print("Actual   :", bot_answer)
                 print("Bleu     : ", bleu)
                 print()
-    print("Count:", count)
-    print("Score:", np.mean(scores))
+    output["score"] = np.mean(scores)
+    return output
 
 
 HoaiAn.reply("local", ":build HoaiAn")
 HoaiAn.reply("local", ":reset")
 HoaiAn.reply("local", ":trace")
 
+
+def evaluate(files):
+    output = [evaluate_file(filepath) for filepath in files]
+    df = pd.DataFrame(output, columns=["talk", "question", "score"])
+    print(df)
+    print("Score:",df["score"].mean())
+
 TEST_FOLDER = "data/hoaian01/raw"
 files = []
 for file in listdir(TEST_FOLDER):
     files.append(join(TEST_FOLDER, file))
-# files = [
-#     "data/hoaian01/raw/conversation.json",
-#     "data/hoaian01/raw/myself.json"
-# ]
-for filepath in files:
-    print("")
-    print(filepath)
-    evaluate(filepath)
+
+evaluate(files)
