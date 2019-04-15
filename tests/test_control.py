7edaccfdc729c6e9cdde9fa65e7d5d2859a3985d
anhv <anhv.ict91@gmail.com>
2019-04-15 13:41:28 +0700

    GH-13: add generate sentences

--- tests/test_control.py
+++ tests/test_control.py
@@ -1,6 +1,14 @@
 from engine.hoaian import HoaiAn
 
-HoaiAn.reply("local", ":build HoaiAn")
-HoaiAn.reply("local", ":reset")
-reply = HoaiAn.reply("loopback", user="anhv")
-print(reply)
+HoaiAn.reply(":build HoaiAn", "local")
+HoaiAn.reply(":reset", "local")
+
+def talk(input_text):
+    print("user:", input_text)
+    reply = HoaiAn.reply(input_text, user="local")
+    print("bot:", reply)
+
+talk("Chào cậu")
+for i in range(10):
+    talk("???")
+
